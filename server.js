e23474479901fb1bda02a6fb5fe308079c09655b
Zayyan Zia <zayyanzia153@gmail.com>
2025-06-28 22:41:38 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,21 +2,21 @@ const express=require('express');
 const app=express();
 const mysql=require('mysql2/promise');
 const db= mysql.createPool({
-host:'localhost',
-user:'root',
-password:'zayyan153',
-database:'user_data'
+host:'',
+user:'',
+password:'',
+database:''
 });
 app.get('/api/world',(req,res,next)=>{
     const name='zayyan zia';
     res.send(name);
 });
-app.post('/api/submit-data',(req,res,next)=>{
+app.post(Routes,(req,res,next)=>{
    const {fullname,email,password}=req.body;
    res.send(fullname,'you have successfully added on database with password');
    db.execute('INSERT INTO users (name,eamil,password) VALUES(?,?,?)',[fullname,email,password]);
 })
-const port=process.env.PORT||3001;
+const port=process.env.PORT||3001;                                                                                                                                                                                                                                                                    
 app.listen(port,()=>{
     console.log(`server running on https://localhost${port}`);
 });
